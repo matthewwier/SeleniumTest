09a77f85edb0663bf7ba5ea6464c12f8dc86b7ea
matthewwier <fjh5919@gmail.com>
2020-01-04 16:34:57 +0100

    Added test

---
+++ src/test/java/testapp/AppTest.java
@@ -0,0 +1,33 @@
+package testapp;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AppTest {
+    private WebDriver driver;
+
+    @Test
+    public void check() {
+        driver.get("http://www.google.pl");
+        driver.findElement(By.xpath("//*[@id=\"gb_70\"]")).click();
+
+        Assert.assertEquals("Zaloguj się", driver.findElement(By.xpath("//*[@id=\"headingText\"]/span")).getText());
+
+    }
+
+    @Before
+    public void beforeTest() {
+        System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\SeleniumDrivers\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    @After
+    public void afterTest() {
+        driver.quit();
+    }
+}
